902ca9dbaa037d976459dde796f960c222a046f6
eojin312 <eojin312@naver.com>
2023-01-27 12:21:22 +0900

    spring Container 로 통합

--- src/main/java/ejlee/tobyspring/TobyspringApplication.java
+++ src/main/java/ejlee/tobyspring/TobyspringApplication.java
@@ -12,18 +12,23 @@ public class TobyspringApplication {
 
     public static void main(String[] args) {
         // 스프링컨테이너 만들기
-        GenericWebApplicationContext genericApplicationContext = new GenericWebApplicationContext(); // 어플리케이션
+        GenericWebApplicationContext genericApplicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+                TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory(); // 추상클래스 -> Embedded Tomcat 을 이용
+
+                WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatherServlet",
+                                    new DispatcherServlet(this))
+                            .addMapping("/*"); // url mapping -> 모든 url 을 검증하겠다
+                }); // ServletContextInitializer 는 서블릿 컨테이너에다가 서블릿 등록하는데 지원해주는
+                webServer.start();
+            }
+        };
+
         genericApplicationContext.registerBean(SimpleHelloService.class); // 빈 등록
         genericApplicationContext.registerBean(HelloController.class); // 빈 등록
-        genericApplicationContext.refresh(); // 초기화
-
-        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory(); // 추상클래스 -> Embedded Tomcat 을 이용
-
-        WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatherServlet",
-                            new DispatcherServlet(genericApplicationContext))
-                    .addMapping("/*"); // url mapping -> 모든 url 을 검증하겠다
-        }); // ServletContextInitializer 는 서블릿 컨테이너에다가 서블릿 등록하는데 지원해주는
-        webServer.start();
+        genericApplicationContext.refresh(); // 스프링 컨테이너 초기화
     }
 }
