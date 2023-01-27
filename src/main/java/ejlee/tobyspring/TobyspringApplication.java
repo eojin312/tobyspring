11138895ef6c0f04a00e05adc0d60d0f01c14b00
eojin312 <eojin312@naver.com>
2023-01-27 12:31:42 +0900

    @Component 스캔, 메타 어노테이션 등록

--- src/main/java/ejlee/tobyspring/TobyspringApplication.java
+++ src/main/java/ejlee/tobyspring/TobyspringApplication.java
@@ -6,6 +6,7 @@ import ejlee.tobyspring.hello.SimpleHelloService;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -13,17 +14,8 @@ import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration // 스프링 구성정보다 설정해주는 것
+@ComponentScan // Component 가 붙어져있는 클래스를 모두 빈으로 등록해줌
 public class TobyspringApplication {
-
-    @Bean
-    public HelloController helloController(HelloService helloService) {
-        return new HelloController(helloService);
-    }
-
-    @Bean
-    public HelloService helloService() {
-        return new SimpleHelloService();
-    };
 
     public static void main(String[] args) {
         // 스프링컨테이너 만들기
--- src/main/java/ejlee/tobyspring/hello/HelloController.java
+++ src/main/java/ejlee/tobyspring/hello/HelloController.java
@@ -1,5 +1,6 @@
 package ejlee.tobyspring.hello;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.Objects;
 
 @RequestMapping("/hello") // dispatherServlet 이 찾을 수 쉽게 클래스단에 적용
+@MyComponent
 public class HelloController {
 
     private final HelloService helloService;
