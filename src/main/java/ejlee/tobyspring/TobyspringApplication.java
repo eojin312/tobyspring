e2b52bbecbe1d93772d330a620b14ddbfd04cf76
eojin312 <eojin312@naver.com>
2023-01-27 11:48:10 +0900

    Dependency Injection 을 통해 스프링이 알아서 해주던 의존성주입 수동으로 해보기

--- src/main/java/ejlee/tobyspring/TobyspringApplication.java
+++ src/main/java/ejlee/tobyspring/TobyspringApplication.java
@@ -57,7 +57,7 @@ public class TobyspringApplication {
                 }
             })
                     .addMapping("/*"); // url mapping -> 모든 url 을 검증하겠다
-        }); // ServletContextInitializer 는 서블릿 컨테이너에다가 서블릿 등록하는데 지원해주는 클래스
+        }); // ServletContextInitializer 는 서블릿 컨테이너에다가 서블릿 등록하는데 지원해주는
         webServer.start();
     }
 }
