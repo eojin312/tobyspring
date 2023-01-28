d2535c1b8bfadac28eb4b1bf9ab0b93b542f08e3
ejlee <eojin312@naver.com>
2023-01-28 23:25:47 +0900

    스프링 어플리케이션 생성하고 빈 등록 + 사용

--- src/main/java/ejlee/tobyspring/hello/SimpleHelloService.java
+++ src/main/java/ejlee/tobyspring/hello/SimpleHelloService.java
@@ -1,12 +1,8 @@
 package ejlee.tobyspring.hello;
 
-import org.springframework.stereotype.Component;
+public class SimpleHelloService {
 
-@Component
-public class SimpleHelloService implements HelloService{
-
-    @Override
-    public String sayHello(String name) {
+    String sayHello(String name) {
         return name;
     }
 }
