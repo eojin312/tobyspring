e4e1f262638550cbf65a1b0fe5e9bce6afd02941
ejlee <eojin312@naver.com>
2023-01-31 23:31:22 +0900

    단위 테스트

--- src/test/java/ejlee/tobyspring/HelloApiTest.java
+++ src/test/java/ejlee/tobyspring/HelloApiTest.java
@@ -3,9 +3,7 @@ package ejlee.tobyspring;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 public class HelloApiTest {
