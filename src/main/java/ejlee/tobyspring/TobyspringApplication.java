9a43dade8502b2c917b60dc14ac6f2b15937ef90
ejlee <eojin312@naver.com>
2023-02-01 00:04:15 +0900

    강의 이어서 보려고

--- src/main/java/ejlee/tobyspring/TobyspringApplication.java
+++ src/main/java/ejlee/tobyspring/TobyspringApplication.java
@@ -5,6 +5,8 @@ import ejlee.tobyspring.hello.MySpringBootAnnotation;
 
 @MySpringBootAnnotation
 public class TobyspringApplication {
+
+    // TODO : 빈 오브젝트 역할과 구분부터
     public static void main(String[] args) {
         MyHelloApplication.run(TobyspringApplication.class, args);
     }
