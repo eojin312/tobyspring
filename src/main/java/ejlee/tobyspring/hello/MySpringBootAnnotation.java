4131754a47a71c26996ae39d4feca4fc95216de6
ejlee <eojin312@naver.com>
2023-02-01 00:02:07 +0900

    합성 어노테이션

---
+++ src/main/java/ejlee/tobyspring/hello/MySpringBootAnnotation.java
@@ -0,0 +1,16 @@
+package ejlee.tobyspring.hello;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME) // java annotation 은 class 라 runtime 시점까지 살아있도록하기위함
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@ComponentScan
+@Configuration
+public @interface MySpringBootAnnotation {
+}
