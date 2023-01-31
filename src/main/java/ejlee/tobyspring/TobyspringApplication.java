9724c8e5ea7082beaf1fe1fc3d69cda8ba5784b4
ejlee <eojin312@naver.com>
2023-02-01 00:03:14 +0900

    합성 어노테이션을 이용해서 코드 지우기

--- src/main/java/ejlee/tobyspring/TobyspringApplication.java
+++ src/main/java/ejlee/tobyspring/TobyspringApplication.java
@@ -1,31 +1,10 @@
 package ejlee.tobyspring;
 
-import ejlee.tobyspring.hello.HelloController;
 import ejlee.tobyspring.hello.MyHelloApplication;
 import ejlee.tobyspring.hello.MySpringBootAnnotation;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 
 @MySpringBootAnnotation
 public class TobyspringApplication {
-
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
-
-
     public static void main(String[] args) {
         MyHelloApplication.run(TobyspringApplication.class, args);
     }
