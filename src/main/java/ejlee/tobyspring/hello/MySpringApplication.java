d0e42030bbf51a840e9569b610cdcb1d40911cd6
ejlee <eojin312@naver.com>
2023-01-31 22:34:11 +0900

    No qualifying bean of type 'org.springframework.boot.web.servlet.server.ServletWebServerFactory' available 뜨는 이유 찾아보기..

--- src/main/java/ejlee/tobyspring/hello/MySpringApplication.java
+++ src/main/java/ejlee/tobyspring/hello/MySpringApplication.java
@@ -4,20 +4,16 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@ComponentScan
+@Configuration
 public class MySpringApplication {
 
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
 
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
 
 
     public static void run(Class<?> applicationClass, String... args) {
